17e33adfc4d758e87310efc2d412c707272539f8
Rabby <fajla.rabby008@gmail.com>
2022-08-23 01:32:33 +0600

    initialize start node and finihsh node

--- src/PathfindingVisualizer/Node/Node.js
+++ src/PathfindingVisualizer/Node/Node.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './Node.css';
 
-class Node extends Component {
-    
-    render()
-     {
-       return <div className='node'></div>;
+export default class Node extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const {isFinish, isStart} = this.props;
+        //tarnary operator with if else
+        const extraClassName = isFinish
+        ? 'node-finish'
+        : isStart
+        ? 'node-start'
+        : '';
+        return <div className={`node ${extraClassName}`}></div>;
     }
 }
 
-export default Node;
+export const DEFAULT_NODE = {
+    row: 0,
+    col: 0,
+};
--- src/PathfindingVisualizer/PathfindingVisualizer.js
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -3,51 +3,69 @@ import Node from './Node/Node';
 import './pathfindingVisualizer.css';
 
 class PathfindingVisualizer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             nodes: [],
         };
     }
 
-    /*creating node with 15 row and 50 column*/   
-    componentDidMount()
-    {
+    /*creating node with 15 row and 50 column*/
+    componentDidMount() {
         const nodes = [];
-        for (let row = 0; row < 15; row++)
-        {
+        for (let row = 0; row < 15; row++) {
             const currentRow = [];
-            for (let col = 0; col < 50; col++)
-            {
-                currentRow.push([]);
+            for (let col = 0; col < 50; col++) {
+                //initialize start and end node with the object currentNode
+                const currentNode = {
+                    col,
+                    row,
+                    isStart: row === 10 && col === 5,
+                    isFinish: row === 10 && col === 45,
+                }
+                currentRow.push(currentNode);
             }
             nodes.push(currentRow);
         }
-        this.setState({nodes})
+        this.setState({ nodes })
     }
 
     render() {
-        const {nodes} = this.state;
+        const { nodes } = this.state;
         console.log(nodes);
-
 
         return (
             <>
-             <button className='btn'>
-          Visualize Dijkstra's Algorithm
-        </button>
-            <div className='grid'>
-                {
-                    nodes.map((row, rowIdx) =>{
-                        return <div>
-                            {row.map((node, nodeIdx) => <Node></Node>)}
-                        </div>
-                    })
-                }
-                
-            </div>
+                <button className='btn'>
+                    Visualize Dijkstra's Algorithm
+                </button>
+
+                <div className='grid'>
+                    {
+                        nodes.map((row, rowIdx) => {
+                            return (
+                                <div key={rowIdx}>
+                                    {
+                                        row.map((node, nodeIdx) => {
+                                            const { isStart, isFinish } = node;
+                                            return (
+                                                //here i passed down properties based on the node (one start and one finish)
+                                                <Node
+                                                    key={nodeIdx}
+                                                    isStart={isStart}
+                                                    isFinish={isFinish}
+                                                    test={'foo'}
+                                                ></Node>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
             </>
-           
         );
     }
 }
